b42d68a9982da88fc5fca344b0fef1d2e1015545
MadCoderr <msrfarooqi@gmail.com>
2022-05-15 22:19:21 +0500

    fix: mob scroll

--- src/App.js
+++ src/App.js
@@ -28,31 +28,33 @@ function App() {
         <Gradient />
       </div>
       <Container>
-        <Navbar />
-        <div className="h-[calc(100vh-158px)] w-full flex flex-col lg:flex-row">
-          <div className="bg-clip bg-img-1 w-full h-1/4 lg:h-full lg:w-1/4 relative ">
-            <span className="center text-white text-2xl font-medium italic  ">
-              Person 1
-            </span>
+        <div className="app-root flex flex-col">
+          <Navbar />
+          {/* h-[calc(100vh-158px)] */}
+          <div className="flex-grow w-full flex flex-col lg:flex-row">
+            <div className="bg-clip bg-img-1 w-full h-1/4 lg:h-full lg:w-1/4 relative ">
+              <span className="center text-white text-2xl font-medium italic  ">
+                Person 1
+              </span>
+            </div>
+            <div className="bg-clip bg-img-2 w-full h-1/4 lg:h-full lg:w-1/4 relative">
+              <span className="center text-white text-2xl font-medium italic">
+                Person 2
+              </span>
+            </div>
+            <div className="bg-clip bg-img-3 w-full h-1/4 lg:h-full lg:w-1/4 relative">
+              <span className="center text-white text-2xl font-medium italic">
+                Person 3
+              </span>
+            </div>
+            <div className="bg-clip bg-img-4 w-full h-1/4 lg:h-full lg:w-1/4 relative">
+              <span className="center text-white text-2xl font-medium italic">
+                Person 4
+              </span>
+            </div>
           </div>
-          <div className="bg-clip bg-img-2 w-full h-1/4 lg:h-full lg:w-1/4 relative">
-            <span className="center text-white text-2xl font-medium italic">
-              Person 2
-            </span>
-          </div>
-          <div className="bg-clip bg-img-3 w-full h-1/4 lg:h-full lg:w-1/4 relative">
-            <span className="center text-white text-2xl font-medium italic">
-              Person 3
-            </span>
-          </div>
-          <div className="bg-clip bg-img-4 w-full h-1/4 lg:h-full lg:w-1/4 relative">
-            <span className="center text-white text-2xl font-medium italic">
-              Person 4
-            </span>
-          </div>
+          <Footer />
         </div>
-
-        <Footer />
       </Container>
     </div>
   );
