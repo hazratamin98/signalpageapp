b371e5a902e46d420053e043cdd3bcd2db05abbf
MadCoderr <msrfarooqi@gmail.com>
2022-05-15 22:22:58 +0500

    style(Logo): set width

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -4,7 +4,7 @@ import Logo from "../image/logo.svg";
 const Navbar = () => {
   return (
     <div className="flex items-center justify-between px-4">
-      <img className="max-w-full" src={Logo} alt="..." />
+      <img className="max-w-full w-[80px] sm:w-auto" src={Logo} alt="..." />
       <button className="border border-white rounded text-white text-base font-medium py-1 px-2">
         Get Started
       </button>
