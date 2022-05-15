ee73228d124cdb399ac56fd5b87d58acc3db2c5d
hazratamin98 <hazratamin98@gmail.com>
2022-05-15 14:59:38 +0500

    footer

--- src/App.js
+++ src/App.js
@@ -2,11 +2,30 @@ import "./App.css";
 import Navbar from "./component/Navbar";
 import Footer from "./component/Footer";
 import Container from "./component/container";
+<link
+  rel="stylesheet"
+  href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+  integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
+  crossorigin="anonymous"
+></link>;
 function App() {
   return (
     <Container>
       <Navbar />
-      <div className="h-screen">Main page</div>
+      <div className="h-screen w-full sm:flex justify-between text-center cursor-pointer text-white ">
+        <div className="clip-your-needful-style bg-red-300 w-1/4">
+          <a>Item 1</a>
+        </div>
+        <div className=" clip-your-needful-style  w-1/4 bg-blue-300">
+          <a>Item 2</a>
+        </div>
+        <div className=" clip-your-needful-style w-1/4 bg-green-400">
+          <a>Item 3</a>
+        </div>
+        <div className="clip-your-needful-style w-1/4 bg-yellow-500">
+          <a>Item 4</a>
+        </div>
+      </div>
 
       <Footer />
     </Container>
--- src/component/Footer.js
+++ src/component/Footer.js
@@ -2,10 +2,12 @@ import React from "react";
 
 function Footer() {
   return (
-    <div className="py-12 bg-red-500">
-      <div>
+    <div className="py-12 bg-[#1E3E36] text-white">
+      <div className="md:flex justify-end gap-10 px-16 ">
         <h1>Contact us </h1>
+        <h1>Privacy Policy </h1>
       </div>
+      <h1 className="flex center justify-center">CopyRight</h1>
     </div>
   );
 }
