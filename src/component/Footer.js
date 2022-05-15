50aa342928f892076703bdc94c0086663646b8c2
hazratamin98 <hazratamin98@gmail.com>
2022-05-15 17:07:33 +0500

    padding left

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
   return (
     <div className="py-12 text-white ">
-      <div className="flex-row md:flex md:justify-end text-center   md:text-end gap-10 font-medium  ">
+      <div className="flex-row md:flex md:justify-end text-center   md:text-end gap-10 font-medium  pl-6 ">
         <h1>Contact us </h1>
         <h1>Privacy Policy </h1>
       </div>
