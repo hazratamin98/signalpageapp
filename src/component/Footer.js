45de498d95310c026099c60bebbd7b0729396840
MadCoderr <msrfarooqi@gmail.com>
2022-05-15 16:22:05 +0500

    fix: layout

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -2,12 +2,12 @@ import React from "react";
 
 function Footer() {
   return (
-    <div className="py-12 bg-[#1E3E36] text-white">
+    <div className="py-12 text-white">
       <div className="md:flex justify-end gap-10 px-16 ">
         <h1>Contact us </h1>
         <h1>Privacy Policy </h1>
       </div>
-      <h1 className="flex center justify-center">CopyRight</h1>
+      <h1 className="flex  justify-center">CopyRight</h1>
     </div>
   );
 }
--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,10 +1,13 @@
 import React from "react";
+import Logo from "../image/logo.svg";
 
 const Navbar = () => {
   return (
-    <div className="py-8  bg-slate-50 shadow-md flex justify-between px-20">
-      <a href="f">Logo</a>
-      <button>LogIn</button>
+    <div className="flex items-center justify-between px-4 lg:px-20">
+      <img className="max-w-full" src={Logo} alt="..." />
+      <button className="border border-white rounded text-white text-base font-medium py-1 px-2">
+        Get Started
+      </button>
     </div>
   );
 };
