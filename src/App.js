fd5bcd35f5e3a2a96be2a1bdab65c1fb76fbfcd3
hazratamin98 <hazratamin98@gmail.com>
2022-05-15 21:00:01 +0500

    text effect

--- src/App.js
+++ src/App.js
@@ -17,22 +17,30 @@ function App() {
         <div className="h-[calc(100vh-158px)] w-full flex flex-col lg:flex-row">
           <div className="bg-clip bg-img-1 w-full h-1/4 lg:h-full lg:w-1/4 relative ">
             <span className="center text-white text-2xl font-medium italic  ">
-              Person 1
+              <p>
+                <a href="prsn1"> Person 1</a>
+              </p>
             </span>
           </div>
           <div className="bg-clip bg-img-2 w-full h-1/4 lg:h-full lg:w-1/4 relative">
             <span className="center text-white text-2xl font-medium italic">
-              Person 2
+              <p>
+                <a href="prsn2"> Person 2</a>
+              </p>
             </span>
           </div>
           <div className="bg-clip bg-img-3 w-full h-1/4 lg:h-full lg:w-1/4 relative">
             <span className="center text-white text-2xl font-medium italic">
-              Person 3
+              <p>
+                <a href="prsn3"> Person 3</a>
+              </p>
             </span>
           </div>
           <div className="bg-clip bg-img-4 w-full h-1/4 lg:h-full lg:w-1/4 relative">
             <span className="center text-white text-2xl font-semibold italic">
-              Person 4
+              <p>
+                <a href="prsn4"> Person 4</a>
+              </p>
             </span>
           </div>
         </div>
